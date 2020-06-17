420439e08635013ecb5f1cbea26fc07a3f123001
TaeVon Lewis <lewistpro@gmail.com>
2020-06-17 05:04:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
-   if phrase != phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
+   elsif phrase != phrase.upcase
       "HUH?! SPEAK UP, SONNY!"
    else "NO, NOT SINCE 1938!"
    end
